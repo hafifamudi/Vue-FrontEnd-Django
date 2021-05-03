d63b99ae0b279936b71346f563f859b8c3fd4f3d
hafifamudi <hafifcyber@gmail.com>
2021-05-03 12:38:42 +0800

    initial commit; setup the vue frontend

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+      <router-link to="/" class="navbar-item"><strong>Django-Shop</strong></router-link>
+
+      <a class="navbar-burger" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+      </div>
+    
+
+    <div class="navbar-menu" id="navbar-menu" :class="{'is-active': showMobileMenu}">
+      <div class="navbar-end">
+        <router-link to="/summber" class="navbar-item">Summer</router-link>
+        <router-link to="/winter" class="navbar-item">Winter</router-link>
+
+        <div class="navbar-item">
+          <div class="buttons">
+            <router-link to="/login" class="button is-light">Log in</router-link>
+
+            <router-link to="/cart" class="button is-success">
+              <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+              <span>Cart</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <section class="section">
+    <router-view/>
+  </section>
+    
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2021</p>
+  </footer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '../node_modules/bulma';
+</style>

ff8e6cadc44f38b700af1ab4a29ad268fa122fb5
hafifamudi <hafifcyber@gmail.com>
2021-05-05 01:21:17 +0800

    add: checkout; fix login feature and detail product

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,17 @@
   <div id="wrapper">
     <nav class="navbar is-dark">
       <div class="navbar-brand">
-      <router-link to="/" class="navbar-item"><strong>Django-Shop</strong></router-link>
+        <router-link to="/" class="navbar-item"><strong>Djackets</strong></router-link>
 
-      <a class="navbar-burger" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-    
 
-    <div class="navbar-menu" id="navbar-menu" :class="{'is-active': showMobileMenu}">
-      <div class="navbar-start">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-start">
           <div class="navbar-item">
             <form method="get" action="/search">
               <div class="field has-addons">
@@ -32,41 +31,49 @@
             </form>
           </div>
         </div>
-        
-      <div class="navbar-end">
-        <router-link to="/shoes-of-the-year" class="navbar-item">Shoes of the Year</router-link>
-        <router-link to="/boots-of-the-year" class="navbar-item">Boots of the Year</router-link>
 
-        <div class="navbar-item">
-          <div class="buttons">
-            <router-link to="/login" class="button is-light">Log in</router-link>
+        <div class="navbar-end">
+          <router-link to="/shoes-of-the-year" class="navbar-item">Summer</router-link>
+          <router-link to="/boots-of-the-year" class="navbar-item">Winter</router-link>
 
-            <router-link to="/cart" class="button is-success">
-              <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-              <span>Cart ({{ cartTotalLength }})</span>
-            </router-link>
+          <div class="navbar-item">
+            <div class="buttons">
+              <template v-if="$store.state.isAuthenticated">
+                <router-link to="/my-account" class="button is-light">My account</router-link>
+              </template>
+
+              <template v-else>
+                <router-link to="/sign-in" class="button is-light">Log in</router-link>
+              </template>
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart ({{ cartTotalLength }})</span>
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
-  <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading }">
+    <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading }">
       <div class="lds-dual-ring"></div>
-  </div>
+    </div>
 
-  <section class="section">
-    <router-view/>
-  </section>
-    
-  <footer class="footer">
-    <p class="has-text-centered">Copyright (c) 2021</p>
-  </footer>
+    <section class="section">
+      <router-view/>
+    </section>
 
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
   </div>
 </template>
 
+
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
@@ -79,6 +86,13 @@ export default {
 
   beforeCreate() {
     this.$store.commit('initializeStore')
+
+    const token = this.$store.state.token
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = "Token " + token
+    } else {
+        axios.defaults.headers.common['Authorization'] = ""
+    }
   },
 
   mounted() {
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
+import store from '../store'
+
+import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
+import SignIn from '../views/SignIn.vue'
+import SignUp from '../views/SignUp.vue'
+import MyAccount from '../views/MyAccount.vue'
+import Success from '../views/Success.vue'
+import Checkout from '../views/Checkout.vue'
 
 const routes = [
   {
@@ -32,9 +39,33 @@ const routes = [
   },
 
   {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+
+  {
     path: '/cart',
     name: 'Cart',
     component: Cart
+  },
+
+  {
+    path: '/cart/success',
+    name: 'Success',
+    component: Success
+  },
+
+  {
+    path: '/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
+    meta: {
+        requireLogin: true
+    }
   },
 
   {
@@ -64,4 +95,12 @@ const router = createRouter({
   routes
 })
 
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'SignIn', query: { to: to.path } });
+  } else {
+    next()
+  }
+})
 export default router
